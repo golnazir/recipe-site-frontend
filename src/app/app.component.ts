e7b41be5f66825aaec48470be1323a42f00e060e
golnazir <golnazirannejad@gmail.com>
2019-05-15 17:13:03 -0700

    Improve css design.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Easy and fun recipes!';
+  title = 'Easy and delicious recipes!';
 }
